6c05b93eb4848ab3bc690fb7b85a1775f418f559
Saulo Moreira <saulo.node@gmail.com>
2024-09-29 21:42:36 -0300

    Windows Conecting proxy in C now

--- toralize/toralize.c
+++ toralize/toralize.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <arpa/inet.h>  
-#include <string.h>     
-#include <unistd.h>    
+#include <winsock2.h>
+#include <string.h>
 #include "toralize.h"
 
-#define reqsize 128 
-#define ressize 128  
-#define PROXYPORT 8080  
-#define PROXY "192.168.1.1"  
-#define USERNAME "user"  
+#define reqsize 128
+#define ressize 128
+#define PROXYPORT 8080
+#define PROXY "192.168.1.1"
+#define USERNAME "user"
+
+void init_winsock() {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Falha ao inicializar o Winsock. Código de erro: %d\n", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+}
 
 Req *request(const char *dstip, const int dstport) {
     Req *req;
@@ -35,9 +42,10 @@ int main(int argc, char *argv[]) {
     char success;
     char tmp[512];
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+    init_winsock();
 
+    if (argc < 3) {
+        fprintf(stderr, "Uso: %s <host> <port>\n", argv[0]);
         return -1;
     }
 
@@ -46,65 +54,66 @@ int main(int argc, char *argv[]) {
 
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0) {
-        perror("socket");
-
+        perror("Erro ao criar socket");
         return -1;
     }
 
     res = (Res *)buf;
 
-
     sock.sin_family = AF_INET;
     sock.sin_port = htons(PROXYPORT);
     sock.sin_addr.s_addr = inet_addr(PROXY);
 
-    if (connect(s, (struct sockaddr *) &sock, sizeof(sock))) {
-        perror("connect");
-
+    if (connect(s, (struct sockaddr *)&sock, sizeof(sock)) != 0) {
+        perror("Erro ao conectar");
         return -1;
     }
 
-    printf("Connected to proxy\n");
+    printf("Conectado ao proxy\n");
     req = request(host, port);
-    write(s, req, reqsize);
+    send(s, (char *)req, reqsize, 0);
 
     memset(buf, 0, ressize);
-    if (read(s, buf, ressize) < 1) {
-        perror("read");
+    if (recv(s, buf, ressize, 0) < 1) {
+        perror("Erro ao ler resposta");
         free(req);
-        close(s);
-    
+        closesocket(s);
         return -1;
     }
 
     res = (Res *)buf;
     success = (res->cd == 90);
     if (!success) {
-        fprintf(stderr, "Unable to traverse"
-        "the proxy, error code: %d\n",
-        res->cd);
-
-        close(s);
+        fprintf(stderr, "Não foi possível atravessar o proxy, código de erro: %d\n", res->cd);
+        closesocket(s);
         free(req);
-
         return -1;
     }
 
-    printf("Successfully connected through the proxy to %s:%d\n", host, port); 
+    printf("Conectado com sucesso através do proxy para %s:%d\n", host, port);
 
     memset(tmp, 0, 512);
     snprintf(tmp, 511,
         "HEAD / HTTP/1.0\r\n"
         "Host: www.example.com\r\n"
-        "\r\n"); 
-    write(s, tmp, strlen(tmp));
+        "\r\n");
+    send(s, tmp, strlen(tmp), 0);
 
     memset(tmp, 0, 512);
-    read(s, tmp, 511); 
+    recv(s, tmp, 511, 0);
     printf("'%s'\n", tmp);
 
-    close(s);
+    closesocket(s);
     free(req);
+
+    WSACleanup();
 
     return 0;
 }
+
+
+//Run: 
+//gcc -o my_proxy_program main.c -lws2_32
+
+//than run:
+//my_proxy_program <host> <port>
--- toralize/toralize.h
+++ toralize/toralize.h
@@ -1,29 +1,21 @@
+#ifndef TORALIZE_H
+#define TORALIZE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-
-//man 2 socket on Terminal
-
-socket()
-connect()
-close()
-htons()
-inet_addr()
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 #define PROXY        "127.0.0.1"
-#define PROXYPORT     9050
-#define USERNAME      "toralize"
-#define reqsie sizeof(struct proxy_request);
-#define ressize sizeof(struct proxy_response);
+#define PROXYPORT    9050
+#define USERNAME     "toralize"
+#define reqsize      sizeof(struct proxy_request)
+#define ressize      sizeof(struct proxy_response)
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
-
 
 struct proxy_request {
     int8 vn;
@@ -38,9 +30,8 @@ typedef struct proxy_request Req;
 struct proxy_response {
     int8 vn;
     int8 cd;
-    int16 _;
-    int32 __;
-
+    int16 reserved;
+    int32 reserved2;
 };
 
 typedef struct proxy_response Res;
@@ -48,7 +39,11 @@ typedef struct proxy_response Res;
 Req *request(const char*, const int);
 int main(int, char**);
 
+#endif // TORALIZE_H
 
 
+//Run: 
+//gcc -o my_proxy_program main.c -lws2_32
 
-
+//than run:
+//my_proxy_program <host> <port>
